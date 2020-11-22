a5fdda840ea7a4ce8c2ece83c6d752082521d6f5
leskinen3594 <rikuto1412@gmail.com>
2020-11-22 21:49:29 +0700

    เพิ่มข้อมูลลงฐานข้อมูล MySQL ตามเงื่อนไขเวลา

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import { getValues } from './firebase/query.js'
 import getConnection from './database.js'
 
+// function เก็บค่าเวลาและวันที่ เป็นแบบ locale
 function startTime(){
     // Format datetime : 2020-11-17 07:31:00
     let today       = new Date()
@@ -12,30 +13,33 @@ function startTime(){
     let s           = today.getSeconds()
     m               = checkFormatTime(m)
     s               = checkFormatTime(s)
-    let date_time   = year + "-" + month + "-" + date + " " + h + ":" + m + ":" + s
-    let time        = h + ":" + m + ":" + s
-    var t = setTimeout(startTime, 1000)
+    var date_time   = year + "-" + month + "-" + date + " " + h + ":" + m + ":" + s
+    var time        = h + ":" + m + ":" + s
+    setTimeout(startTime, 1000)
     // console.log(date_time);
     return [ date_time, time ]
 }
 
+// function จัดรูปแบบของหน่วยเวลา นาทีและวินาที
 function checkFormatTime(i) {
     if (i < 10) {i = "0" + i};  // fomat if m < 10 || s < 10 = 00...09
     return i;
 }
 
 setInterval(() => {
-    var people_total    = getValues[0]
-    var cars_total      = getValues[1]
+    // รับข้อมูลจาก query.js
+    let people_total    = getValues[0]
+    let cars_total      = getValues[1]
     console.log(`people = ${people_total}`);
     console.log(`cars = ${cars_total}`);
 
-    var date_time = startTime()[0]
-    var time      = startTime()[1]    
+    // เรียกใช้ function startTime เพื่อรับค่าวันที่และเวลามาเก็บไว้ในตัวแปร
+    let date_time = startTime()[0]
+    let time      = startTime()[1]    
     console.log(`date_time = ${date_time}`);
 
-    var sql = `INSERT INTO total(total_id, total_people, total_car, date) VALUES(${null}, ${people_total}, ${cars_total}, "${date_time}")`
-    if("13:00:00" === time){    //UTC 13.00 เวลาไทยคือ 20.00(UTC/GMT +7)
+    let sql = `INSERT INTO total(total_id, total_people, total_car, date) VALUES(${null}, ${people_total}, ${cars_total}, "${date_time}")`
+    if("13:00:00" === time){    //13.00(UTC +0) เวลาไทยคือ 20.00(UTC/GMT +7)
         getConnection.query(sql, (err, res) => {
             !err ? console.log("Insert Successfully. Increase id: " + res.insertId) : console.log("Insert " + err);
         })
